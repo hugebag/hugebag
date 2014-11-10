c178f68cf38238095fe80518bfc1939c8b8201d3
Alex Man <alex@hugebag.com>
2014-11-10 11:48:53 -0800

    include the module

--- lib/hugebag/railtie.rb
+++ lib/hugebag/railtie.rb
@@ -1,10 +1,12 @@
 require 'hugebag/view_helpers'
+require 'hugebag/controller_helpers'
 require 'hugebag/migration_helpers'
 
 module Hugebag
   class Railtie < Rails::Railtie
     initializer 'railtie.configure_rails_initialization' do
       ActionView::Base.send :include, ViewHelpers
+      ActionController::Base.send :include, ControllerHelpers
       ActiveRecord::Migration.send :include, MigrationHelpers
     end
   end
