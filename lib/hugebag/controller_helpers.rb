cbf4492f56ebfbbaeb35d28cf455cbeee843da7f
Alex Man <alex@hugebag.com>
2014-11-13 21:02:15 -0800

    new methods for using respond_with in the controllers

--- lib/hugebag/controller_helpers.rb
+++ lib/hugebag/controller_helpers.rb
@@ -13,9 +13,29 @@ module ControllerHelpers
                                                            :model_name => model_class.model_name.human)
   end
 
+  def set_flash_notice_for_create_success(model_class)
+    # TODO Put the key in a locale file
+    set_flash_notice(:create_model_success, model_class)
+  end
+
+  def set_flash_notice_for_update_success(model_class)
+    # TODO Put the key in a locale file
+    set_flash_notice(:update_model_success, model_class)
+  end
+
+  def set_flash_notice_for_destroy_success(model_class)
+    # TODO Put the key in a locale file
+    set_flash_notice(:destroy_model_success, model_class)
+  end
+
   private
 
   def model_name(model_instance)
     model_instance.class.model_name.human
+  end
+
+  def set_flash_notice(message_sym, model_class)
+    model_name = model_class.model_name.human
+    flash[:notice] = t(message_sym, :model_name => model_name)
   end
 end
